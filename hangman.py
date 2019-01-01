6cd54159b3a386bfbaddd2714c271b6fade859b6
M-Mukta <muktamayee55@gmail.com>
2019-01-01 15:44:01 +0530

    updated

--- hangman.py
+++ hangman.py
@@ -11,7 +11,6 @@ s1=["p y t h o n","m u k t a","s t r i n g","h a n g m a n"]
 s=random.choice(s1)
 guess=""
 r=0
-print(len(s))
 while r<len(s):
     if s[r] != " ":
         guess=guess+"_"
@@ -23,74 +22,107 @@ cnt=0
 arr=[]
 l=[]
 cnt1=0
+font3 = pygame.font.SysFont("Times New Roman", 24)
+
 def display():
-    pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
-    pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
-    pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
-    pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
     i=25
     j=0
     for x in string.ascii_lowercase:
         l.append(x)
-        text2 = font.render(x,True,(255,255,255))
-        arr.append(pygame.Rect(pygame.draw.rect(win,(255,255,255),(30+i,430,25,25),2)))
-        win.blit(text2,(40+i,430))
+        
+        text2 = font3.render(x,True,(255,255,255))
+        arr.append(pygame.Rect(pygame.draw.rect(win,(255,255,255),(30+i,485,25,25),2)))
+        win.blit(text2,(40+i,485))
         j=j+1
         i=i+30
     
     pygame.display.update()
     
-    
-
-font = pygame.font.SysFont("Times New Roman", 24)
+font = pygame.font.SysFont("Times New Roman", 26)
 text = font.render(guess, True, (255, 255, 255))
 win.blit(text,(230,380))
 
 while True:
-    
-    display()
+    font1 = pygame.font.SysFont("Times New Roman", 36)
+    name = font1.render("HANGMAN", True, (255, 255, 255))
+    win.blit(name,(350,50)) 
     if cnt1==0:
+        start_text = font.render("Start guessing", True, (255, 255, 255))
+        win.blit(start_text,(200,100))
+    display()
+    if cnt==1:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.display.update()
+                            
+    if cnt==2:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.display.update()
+    if cnt==3:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.draw.circle(win,(248,248,255),(250,210),25,5)
+        pygame.display.update()
+    if cnt==4:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.draw.circle(win,(248,248,255),(250,210),25,5)
+        pygame.draw.line(win,(248,248,255),(250,235),(250,265),5)
+        pygame.display.update()
+    if cnt==5:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.draw.circle(win,(248,248,255),(250,210),25,5)
+        pygame.draw.line(win,(248,248,255),(250,235),(250,265),5)
+        pygame.draw.line(win,(248,248,255),(250,247),(225,265),5)
+        pygame.display.update()
+    if cnt==6:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.draw.circle(win,(248,248,255),(250,210),25,5)
+        pygame.draw.line(win,(248,248,255),(250,235),(250,265),5)
+        pygame.draw.line(win,(248,248,255),(250,247),(225,265),5)
+        pygame.draw.line(win,(248,248,255),(250,247),(275,265),5)
+    if cnt==7:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
+        pygame.draw.circle(win,(248,248,255),(250,210),25,5)
+        pygame.draw.line(win,(248,248,255),(250,235),(250,265),5)
+        pygame.draw.line(win,(248,248,255),(250,247),(225,265),5)
+        pygame.draw.line(win,(248,248,255),(250,247),(275,265),5)
+        pygame.draw.line(win,(248,248,255),(250,265),(225,300),5)
+    if cnt==8:
+        pygame.draw.line(win,(248,248,255),(100,150),(100,350),5)
+        pygame.draw.line(win,(248,248,255),(100,150),(300,150),5)
+        pygame.draw.line(win,(248,248,255),(70,350),(130,350),5)
+        pygame.draw.line(win,(248,248,255),(250,150),(250,185),5)
         pygame.draw.circle(win,(248,248,255),(250,210),25,5)
         pygame.draw.line(win,(248,248,255),(250,235),(250,265),5)
         pygame.draw.line(win,(248,248,255),(250,247),(225,265),5)
         pygame.draw.line(win,(248,248,255),(250,247),(275,265),5)
         pygame.draw.line(win,(248,248,255),(250,265),(225,300),5)
         pygame.draw.line(win,(248,248,255),(250,265),(275,300),5)
-        pygame.display.update()
-    if cnt==1:
-        pygame.draw.circle(win,(248,248,255),(250,210),25,2)
-        pygame.draw.line(win,(248,248,255),(250,235),(250,265),2)
-        pygame.draw.line(win,(248,248,255),(250,247),(225,265),2)
-        pygame.draw.line(win,(248,248,255),(250,247),(275,265),2)
-        pygame.draw.line(win,(248,248,255),(250,265),(225,300),2)
-        pygame.display.update()
-                            
-    if cnt==2:
-        pygame.draw.circle(win,(248,248,255),(250,210),25,2)
-        pygame.draw.line(win,(248,248,255),(250,235),(250,265),2)
-        pygame.draw.line(win,(248,248,255),(250,247),(225,265),2)
-        pygame.draw.line(win,(248,248,255),(250,247),(275,265),2)
-        pygame.display.update()
-    if cnt==3:
-        pygame.draw.circle(win,(248,248,255),(250,210),25,2)
-        pygame.draw.line(win,(248,248,255),(250,235),(250,265),2)
-        pygame.draw.line(win,(248,248,255),(250,247),(225,265),2)
-        pygame.display.update()
-    if cnt==4:
-        pygame.draw.circle(win,(248,248,255),(250,210),25,2)
-        pygame.draw.line(win,(248,248,255),(250,235),(250,265),2)
-        pygame.display.update()
-    if cnt==5:
-        pygame.draw.circle(win,(248,248,255),(250,210),25,2)
-        pygame.display.update()
-    if cnt==6:
-        text3 = font.render("Game Over!", True, (255, 255, 255))
-        win.blit(text3,(380,380))
+        text3 = font.render(" WRONG ! Game Over!!!", True, (255, 0, 0))
+        win.blit(text3,(420,380))
         pygame.display.update()
         break
     if guess==s:
-        text4 = font.render("WORD FOUND", True, (255, 255, 255))
-        win.blit(text4,(380,380))
+        text4 = font.render("  CORRECT ", True, (0, 255, 0))
+        win.blit(text4,(420,380))
         pygame.display.update()
         break
     for event in pygame.event.get():
@@ -111,12 +143,11 @@ while True:
                     else:
                         cnt=cnt+1    
                         cnt1=cnt1+1
-                    print(guess,cnt)
                     screen = pygame.display.get_surface()
                     screen.fill(pygame.Color("black")) 
                     text1 = font.render(guess, True, (255, 255, 255))
                     win.blit(text1,(230,380))
                     pygame.display.update()
     
-pygame.time.delay(1000)                
+pygame.time.delay(10000)                
 pygame.quit()
